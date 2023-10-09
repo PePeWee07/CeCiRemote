3396f10555890e888b5788a0633f2de3572ad55d
Pepe <pepewee07@gmail.com>
2023-10-09 14:05:55 -0500

    boton espera a termina talk app

--- src/app/preguntas/preguntas.page.ts
+++ src/app/preguntas/preguntas.page.ts
@@ -10,6 +10,9 @@ import { Howl } from 'howler';
 export class PreguntasPage implements OnInit {
 
   private sounds: { [key: string]: Howl } = {};
+  isPlaying = false;
+  selectedButton: string | null = null;
+
 
   constructor(private ceciTalkService: CeciTalkService) {
     this.setupSounds();
@@ -28,7 +31,6 @@ export class PreguntasPage implements OnInit {
     this.sounds['8'] = new Howl({ src: ['../../assets/audios/8iniciativa_PG.mp3'] });
     this.sounds['9'] = new Howl({ src: ['../../assets/audios/9esfuerzos_PG.mp3'] });
     this.sounds['10'] = new Howl({ src: ['../../assets/audios/10volveraVer_PG.mp3'] });
-    // Agrega más archivos de audio según sea necesario
 
     // Configura eventos 'end' para cada instancia de Howl
     for (const key in this.sounds) {
@@ -36,17 +38,24 @@ export class PreguntasPage implements OnInit {
         const sound = this.sounds[key];
         sound.on('end', () => {
           console.log(`El audio ${key} ha terminado de reproducirse.`);
-          // Puedes realizar acciones adicionales cuando el audio ha terminado aquí
+          // realizar acciones cuando termine la reproducción
           this.CeCiTalk(key);
+          this.isPlaying = false; // Marca que la reproducción ha terminado
         });
       }
     }
   }
 
   playAudio(audioKey: string) {
+    // Verifica si ya se está reproduciendo un audio
+    if (this.isPlaying) {
+      return;  // No hagas nada si ya se está reproduciendo un audio
+    }
     // Iniciar la reproducción del audio por su clave
     const sound = this.sounds[audioKey];
     if (sound) {
+      this.isPlaying = true;
+      this.selectedButton = audioKey; //btn selecionado para reproducir
       sound.play();
     }
   }
